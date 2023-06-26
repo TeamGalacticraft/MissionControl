77848325516732060b3cdb18e3fee60e391b03fd
Alex Long <alexlong2001@outlook.com>
2023-06-26 17:19:18 +0100

    removed categories

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,8 +1,7 @@
 import blockContent from './blockContent'
-import category from './category'
 import post from './post'
 import lineBreak from './lineBreak'
 import youtube from './youtube'
 import gallery from './gallery'
 
-export const schemaTypes = [gallery, post, category, blockContent, lineBreak, youtube]
+export const schemaTypes = [gallery, post, blockContent, lineBreak, youtube]
